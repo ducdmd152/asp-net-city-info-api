d6a026fd9f201f2958d5608e6dc569d15e2bc4ac
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-28 20:39:01 +0700

    Added Controllers + Models

---
+++ CityInfo.API/CityDataStore.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CityDataStore
+    {
+        public List<CityDTO> Cities { get; set; }
+        public static CityDataStore Instance { get; } = new CityDataStore();
+
+        public CityDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO()
+                {
+                    Id = 1,
+                    Name = "Ha Noi",
+                    Description = "The captial of Vietnam."
+                },
+                new CityDTO()
+                {
+                    Id = 2,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDTO()
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The city of love."
+                },
+                new CityDTO()
+                {
+                    Id = 4,
+                    Name = "Tokyo",
+                    Description = "The bustling metropolis."
+                },
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController] // no required but more significant
+    [Route("api/cities")]
+    public class CityController : ControllerBase
+    {
+        [HttpGet] // HttpGet("api/cities")
+        public JsonResult GetCitites()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                   new { id = 1, Name = "New York City" },
+                   new { id = 2, Name = "Antwerp" }
+                });
+        }
+    }
+}
