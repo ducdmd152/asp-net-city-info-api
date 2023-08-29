0cefdd1db7153d04169a767ce9a6adb9c474a5a7
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-08-29 11:25:10 +0700

    Added File API Handler

---
+++ CityInfo.API/Controllers/FileController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider fileExtensionContentTypeProvider;
+        public FileController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider) {
+            this.fileExtensionContentTypeProvider = fileExtensionContentTypeProvider;
+        }
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            var pathToFile = "getting-started-with-rest-slides.pdf";
+            
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType
+                ))
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
